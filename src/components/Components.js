51cd63e026533be3ae1569d8fe3cde6f7a0ff723
farisubuntu <farisubuntu@gmail.com>
2023-12-21 21:12:04 +0300

    fix

--- src/components/Components.js
+++ src/components/Components.js
@@ -1,6 +1,5 @@
 export { default as Header } from "./Header/Header";
 export { default as Footer } from "./Footer/Footer";
-export { default as Hero } from "./Hero/Hero";
 export { default as Lesson } from "./Lesson/Lesson";
 export { default as Vocabulary } from "./Vocabulary/Vocabulary";
 export { default as Category } from "./Category/Category";
